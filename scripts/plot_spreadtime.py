632d88f2abe09e077c82377fbe8cc3c8cf735021
Yu-hsuan Shih <yoyoshih13@gmail.com>
2019-11-11 10:02:49 -0500

    codes to handle subprocess error

--- scripts/plot_spreadtime.py
+++ scripts/plot_spreadtime.py
@@ -7,14 +7,29 @@ from matplotlib import ticker
 def main():
 	d = datetime.datetime.today()
 	date = d.strftime('%m%d')
+	nuptstype = 2
+	tol = 1e-7
 
 	result_dir="../results/"
-	t_cufinufft = np.load(result_dir+'workergpu14_cufinufft_spread_'+date+'.npy')
-	t_finufft   = np.load(result_dir+'worker1160_finufft_spread_'+date+'.npy')
-	t_cunfft    = np.load(result_dir+'workergpu14_cunfft_spread_'+date+'.npy')
+	t_cufinufft = np.load(result_dir+'workergpu31_cufinufft_spread_3d_t'+\
+		str(nuptstype)+'_['+str(tol)+']_'+date+'.npy')
+	t_cufinufft_2 = np.load(result_dir+'workergpu18_cufinufft_spread_3d_t'+\
+		str(nuptstype)+'_['+str(tol)+']_'+'maxsubprob1024_'+date+'.npy')
+	t_finufft   = np.load(result_dir+'worker1047_finufft_spread_3d_t'+\
+		str(nuptstype)+'_['+str(tol)+']_'+date+'.npy')
+	t_cunfft    = np.load(result_dir+'workergpu31_cunfft_spread_3d_t'+\
+		str(nuptstype)+'_['+str(tol)+']_'+date+'.npy')
 
-	npzfile = np.load(result_dir+'param_'+date+'.npz')
-	dim = 2
+	npzfile = np.load(result_dir+'param_['+str(tol)+']_'+date+'.npz')
+	dim = 3
+
+	## Create x label
+	xlabel = ''
+	for i in range(dim):
+		xlabel = xlabel+'N'+str(i+1)
+		if i+1 < dim:
+			xlabel = xlabel+'='
+
 	density_list = npzfile['density']
 	tol_list     = npzfile['tol']
 	N1_list      = npzfile['n1']
@@ -24,17 +39,21 @@ def main():
 			M = [pow(n1,dim) for n1 in N1_list]
 			fig, ax= plt.subplots(1,2,figsize=(15, 6))
 
-			w = 0.2
+			w = 0.1
 			x = np.array(range(len(N1_list)))
 
-			ax[0].bar(x, M/t_finufft[d,t,:],w, color="darkviolet",label='FINUFFT (24 threads)')
-			ax[0].bar(x+w, M/t_cunfft[d,t,:],w, color="violet",label='cuNFFT')
-			ax[0].bar(x+2*w, M/t_cufinufft[d,t,:],w, color="deeppink",label='cuFINUFFT')
+			ax[0].bar(x, M/t_finufft[d,t,:],w, color="red",label='FINUFFT (28 threads)')
+			ax[0].bar(x+w, M/t_cunfft[d,t,:],w, color="orange",label='cuNFFT')
+			ax[0].bar(x+2*w, M/t_cufinufft[d,t,:],w, color="green",\
+				label='cuFINUFFT, maxsubprob=32768')
+			if nuptstype == 2:
+				ax[0].bar(x+3*w, M/t_cufinufft_2[d,t,:],w, color="blue",\
+					label='cuFINUFFT, maxsubprob=1024')
 			formatter = ticker.ScalarFormatter()
 			formatter.set_scientific(True)
 			formatter.set_powerlimits((-1,1))
 
-			ax[0].set_xlabel('nf1=nf2')
+			ax[0].set_xlabel(xlabel)
 			ax[0].set_title('Throughput (#NU pts/s)')
 			ax[0].set_xticks(x+1.5*w)
 			ax[0].set_xticklabels(N1_list)
@@ -44,16 +63,24 @@ def main():
 
 			ax[1].axhline(y=1, linestyle='--', color='k')
 			ax[1].set_title('Speed up (s/s_FINUFFT)')
-			ax[1].plot(x, t_finufft[d,t,:]/t_cunfft[d,t,:],'-o',color='violet',label='cuNFFT')
-			ax[1].plot(x, t_finufft[d,t,:]/t_cufinufft[d,t,:],'-o',color='deeppink',label='cuFINUFFT')
+			ax[1].plot(x, t_finufft[d,t,:]/t_cunfft[d,t,:],'-o',color='orange',label='cuNFFT')
+			ax[1].plot(x, t_finufft[d,t,:]/t_cufinufft[d,t,:],'-o',color='green',\
+				label='cuFINUFFT, maxsubprob=32768')
+			if nuptstype == 2:
+				ax[1].plot(x, t_finufft[d,t,:]/t_cufinufft_2[d,t,:],'-o',color='blue',\
+					label='cuFINUFFT, maxsubprob=1024')
 			ax[1].set_xticks(x)
 			ax[1].set_xticklabels(N1_list)
-			ax[1].set_xlabel('nf1=nf2')
+			ax[1].set_xlabel(xlabel)
 			ax[1].set_ylim(bottom=0)
 			ax[1].grid()
 			ax[1].legend(loc=0)
-			fig.suptitle('Uniform distributed pts, Single Precision, tol='+str(tol)+'(ns ='+str(-np.log10(tol)+1)+'), density='+str(density), fontsize=15)
-			fig.savefig('../plots/density'+format(density, ".2e")+'_'+date+'.png')
+			
+			if nuptstype == 1:
+				fig.suptitle('Uniform distributed pts ('+str(dim)+'D), Single Precision, tol='+str(tol)+'(ns ='+str(-np.log10(tol)+1)+'), density='+str(density), fontsize=15)
+			if nuptstype == 2:
+				fig.suptitle('Sphere quad pts ('+str(dim)+'D), Single Precision, tol='+str(tol)+'(ns ='+str(-np.log10(tol)+1)+'), density='+str(density), fontsize=15)
+			fig.savefig('../plots/3d/density'+format(density, ".2e")+'_3d_t'+str(nuptstype)+'_+tol_'+str(tol)+'_'+date+'.png')
 	plt.show()
 if __name__== "__main__":
 	main()
--- scripts/spreadtime.py
+++ scripts/spreadtime.py
@@ -25,18 +25,24 @@ def get_hostname():
 def main(OUTPUT, has_gpu=False, hostname=None, resultdir=None):
 	d = datetime.datetime.today()
 	date = d.strftime('%m%d')
-	finufft_filename=(resultdir+hostname+'_finufft_spread_'+date)
-	cufinufft_filename=(resultdir+hostname+'_cufinufft_spread_'+date)
-	cunfft_filename=(resultdir+hostname+'_cunfft_spread_'+date)
 
-	nupts_distr=1
-	dim=2
+	nupts_distr = 2
+	tol_totry   = [1e-3] #1e-14, 1e-10, 1e-6 , 1e-2
+
+	finufft_filename=(resultdir+hostname+'_finufft_spread_3d_t'+str(nupts_distr)+\
+		'_'+str(tol_totry)+'_'+date)
+	cufinufft_filename=(resultdir+hostname+'_cufinufft_spread_3d_t'+str(nupts_distr)+\
+		'_'+str(tol_totry)+'_maxsubprob1024_'+date)
+	cunfft_filename=(resultdir+hostname+'_cunfft_spread_3d_t'+str(nupts_distr)+'_'\
+		+str(tol_totry)+'_'+date)
+
+	dim=3
 	reps=5
-	density_totry = [1] #0.1, 1, 10
-	tol_totry     = [1e-7] #1e-14, 1e-10, 1e-6 , 1e-2
-	N1_totry      = [64, 128, 256, 512, 1024, 2048] #128, ... ,4096 
+	density_totry = [0.1, 1] #0.1, 1, 10
+	N1_totry      = [16, 32, 64, 128, 256] #128, ... ,4096 
 	if OUTPUT is True:
-		np.savez(resultdir+'param_'+date, density=density_totry, tol=tol_totry, n1=N1_totry)
+		np.savez(resultdir+'param_'+str(tol_totry)+'_'+date, density=density_totry, tol=tol_totry, 
+			n1=N1_totry)
 
 	finufft_spread   = np.zeros([len(density_totry), len(tol_totry), len(N1_totry)])
 	if has_gpu is True:
@@ -55,40 +61,57 @@ def main(OUTPUT, has_gpu=False, hostname=None, resultdir=None):
 					N2=N1
 				if(dim>2):
 					N3=N1
-				M = int(N1*N2*N3*density)
-				print('(N1, N2, N3, M, tol)=(%d, %d, %d, %d, %f)' %(N1, N2, N3, M, tol))
+				M = int(N1*N2*N3*8*density)
+				print('(den, N1, N2, N3, M, tol)=(%f, %d, %d, %d, %d, %e)' %(density, N1, N2, N3, M, tol))
 				for nn in range(reps):
 					tt = 0.0
-					finufft_output=subprocess.check_output(["./finufft",\
-						str(nupts_distr),str(dim),str(N1),str(N2),str(N3),\
-						str(M),str(tol)], cwd="../").decode("utf-8")
-					finufft_t = float(find_between(finufft_output, \
-						"spread (ier=0):", "s"))
-					finufft_tnow = min(finufft_tnow,finufft_t)
+					if has_gpu is not True:
+						finufft_output=subprocess.check_output(["./finufft",\
+							str(nupts_distr),str(dim),str(N1),str(N2),str(N3),\
+							str(M),str(tol)], cwd="../").decode("utf-8")
+						finufft_t = float(find_between(finufft_output, \
+							"spread (ier=0):", "s"))
+						finufft_tnow = min(finufft_tnow,finufft_t)
 
 					if has_gpu is True:
-						cufinufft_output=subprocess.check_output(["./cufinufft",\
-							str(nupts_distr),str(dim),str(N1),str(N2),str(N3),\
-							str(M),str(tol)], cwd="../").decode("utf-8")
-						cunfft_output=subprocess.check_output(["./cunfft",\
-							str(nupts_distr),str(dim),str(N1),str(N2),str(N3),\
-							str(M),str(tol)], cwd="../").decode("utf-8")
-						cufinufft_t = float(find_between(cufinufft_output, \
-							"Spread (2)", "s"))
+						try:
+							cufinufft_output=subprocess.check_output(["./cufinufft",\
+								str(nupts_distr),str(dim),str(N1),str(N2),str(N3),\
+								str(M),str(tol)], cwd="../").decode("utf-8")
+						except subprocess.CalledProcessError as e:
+							cufinufft_output=None
+						try:
+							cunfft_output=subprocess.check_output(["./cunfft",\
+								str(nupts_distr),str(dim),str(N1),str(N2),str(N3),\
+								str(M),str(tol)], cwd="../").decode("utf-8")
+						except subprocess.CalledProcessError as e:
+							cunfft_output=None
+						if cufinufft_output is not None:
+							cufinufft_t = float(find_between(cufinufft_output, \
+								"Spread (2)", "s")) + \
+								float(find_between(cufinufft_output, \
+                            	"Setup Subprob properties", "s"))
+						else:
+							cufinufft_t = float('Inf')
+						if cunfft_output is not None:
+							cunfft_t = float(find_between(cunfft_output, \
+								"Spread:", "s"))
+						else:
+							cunfft_t = float('Inf')
 						cufinufft_tnow = min(cufinufft_tnow,cufinufft_t)
-						cunfft_t = float(find_between(cunfft_output, \
-							"Spread:", "s"))
 						cunfft_tnow = min(cunfft_tnow,cunfft_t)
 
-				finufft_spread[d,t,n]   = finufft_tnow
+				if has_gpu is not True:
+					finufft_spread[d,t,n]   = finufft_tnow
 				if has_gpu is True:
 					cufinufft_spread[d,t,n] = cufinufft_tnow
 					cunfft_spread[d,t,n]   = cunfft_tnow
 	if OUTPUT is True:
-		np.save(finufft_filename+'.npy', finufft_spread)
 		if has_gpu is True:
 			np.save(cufinufft_filename+'.npy', cufinufft_spread)
 			np.save(cunfft_filename+'.npy', cunfft_spread)
+		else:
+			np.save(finufft_filename+'.npy', finufft_spread)
 
 if __name__== "__main__":
 	HAS_GPU = has_gpu()
